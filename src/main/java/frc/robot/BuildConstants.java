429ae3420c418742498241ecdec72f19a85e24cd
DiegoLolzano <diegolozanor66@gmail.com>
2025-01-29 18:50:28 -0600

    Returned to normal code structure

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 13;
-  public static final String GIT_SHA = "96d6a01d01b360193884965a9a0f4c0012f717eb";
-  public static final String GIT_DATE = "2025-01-27 01:09:17 CST";
+  public static final int GIT_REVISION = 14;
+  public static final String GIT_SHA = "66f1a8dab0e48081ad2f38d3b565bdcc7e26dc16";
+  public static final String GIT_DATE = "2025-01-27 16:54:26 CST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-27 16:25:07 CST";
-  public static final long BUILD_UNIX_TIME = 1738016707125L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-01-29 18:49:04 CST";
+  public static final long BUILD_UNIX_TIME = 1738198144397L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
