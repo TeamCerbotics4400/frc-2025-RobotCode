9cd3b5faab980ff99969e3a3a5ecbbbd0a2cad5d
SvenXD <Romanhernandezsosa1@gmail.com>
2025-03-04 00:28:16 -0600

    Final commit for hueneme

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc-2025-code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 78;
-  public static final String GIT_SHA = "f5b7542bcf556006c82d24074e58f7ee2da1ba73";
-  public static final String GIT_DATE = "2025-03-02 16:41:24 CST";
+  public static final int GIT_REVISION = 79;
+  public static final String GIT_SHA = "987148341d43ab484502d57758ad2bbf87baabb4";
+  public static final String GIT_DATE = "2025-03-02 18:17:08 CST";
   public static final String GIT_BRANCH = "event_HPR";
-  public static final String BUILD_DATE = "2025-03-02 16:41:25 CST";
-  public static final long BUILD_UNIX_TIME = 1740955285369L;
+  public static final String BUILD_DATE = "2025-03-02 18:17:08 CST";
+  public static final long BUILD_UNIX_TIME = 1740961028455L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
