75b67a2efb504a1530a204cf8f4d1341d2190f24
SvenXD <Romanhernandezsosa1@gmail.com>
2025-03-24 00:28:26 -0600

    Changes after laguna

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc-2025-code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 149;
-  public static final String GIT_SHA = "2f03f153374aee50ad29e44d39d69fd1e0623f18";
-  public static final String GIT_DATE = "2025-03-21 19:05:39 CDT";
+  public static final int GIT_REVISION = 150;
+  public static final String GIT_SHA = "6d93f802b6a544c59914ca16d9f72bdd17c14bda";
+  public static final String GIT_DATE = "2025-03-22 11:09:50 CDT";
   public static final String GIT_BRANCH = "event_laguna";
-  public static final String BUILD_DATE = "2025-03-22 11:09:16 CDT";
-  public static final long BUILD_UNIX_TIME = 1742659756561L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-03-22 11:09:51 CDT";
+  public static final long BUILD_UNIX_TIME = 1742659791174L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
