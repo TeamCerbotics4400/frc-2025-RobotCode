5033d0185a8c0e0d35b990a1fb27295e5a9fdae7
SvenXD <Romanhernandezsosa1@gmail.com>
2025-04-04 00:37:35 -0600

    4 Coral autonomous consistent

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc-2025-code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 122;
-  public static final String GIT_SHA = "8c2d991382c20338baf7905069c07236aa0bb5c3";
-  public static final String GIT_DATE = "2025-04-03 19:04:30 CDT";
+  public static final int GIT_REVISION = 125;
+  public static final String GIT_SHA = "ea83c58c88f0e4d616381f9f9332b080b9d39daf";
+  public static final String GIT_DATE = "2025-04-04 00:00:23 CDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-04-03 23:44:10 CDT";
-  public static final long BUILD_UNIX_TIME = 1743741850113L;
+  public static final String BUILD_DATE = "2025-04-04 00:12:50 CDT";
+  public static final long BUILD_UNIX_TIME = 1743743570675L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
