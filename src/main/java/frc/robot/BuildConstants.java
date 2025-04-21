479de2f213575e27ad8cb2ef1c651ca49d98f42e
SvenXD <Romanhernandezsosa1@gmail.com>
2025-04-21 10:12:16 -0600

    Final commit after worlds

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "frc-2025-code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 157;
-  public static final String GIT_SHA = "42cd1304f0505991eb1beae4cb09b201471f4879";
-  public static final String GIT_DATE = "2025-04-18 23:32:03 CDT";
+  public static final int GIT_REVISION = 158;
+  public static final String GIT_SHA = "2652e70c702a2a7659f9cb595f9763ce75478908";
+  public static final String GIT_DATE = "2025-04-19 08:08:50 CDT";
   public static final String GIT_BRANCH = "event_worlds";
-  public static final String BUILD_DATE = "2025-04-19 07:58:53 CDT";
-  public static final long BUILD_UNIX_TIME = 1745067533291L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-04-19 08:08:51 CDT";
+  public static final long BUILD_UNIX_TIME = 1745068131019L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
